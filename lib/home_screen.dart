1a3d4b6767c6dc6a78f2aa339a7a4814a3f4b8f6
AbdulSamad <smdafg@gmail.com>
2024-08-16 03:35:40 +0430

    16th Commit Dialog show when exiting the app

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -24,20 +24,20 @@ The Swedish brand also says that this car will do over 310mph, however this does
         'assets/images/hor_bugatti.jpg', '1577 horsepower', '''
 Those who grew up in the 2000s will probably associate the fastest car in the world with the legendary Bugatti Veyron. Well that car’s spirit has been carried over to the Bugatti Chiron Supersport 300+; an 8.0-litre, quad-turbocharged, 1,600hp mega machine which will do, as the name suggests, over 300mph.
 Specifically, it did 304mph when the French firm unleashed it at Volkswagen’s Ehra-Lessien test track. The Chiron 300+ doesn’t hold the official world record though, as it only did the run in one direction. To break the record you have to do it twice in opposite directions, which Bugatti decided not to do for some reason. A replacement for the Chiron is imminent, so maybe that car will be even faster still.'''),
-    Car('SSC Tuatara', '331MPH', 'twin-turbocharged V8 engine', 'assets/images/hor_ssc_tuatara.jpg',
-        '1350 hp', '''
+    Car('SSC Tuatara', '331MPH', 'twin-turbocharged V8 engine',
+        'assets/images/hor_ssc_tuatara.jpg', '1350 hp', '''
 While the Bugatti Chiron’s complicated W16 engine is mightily impressive, sometimes you just can’t beat a whopping great V8 with a couple of turbos strapped to it. The SSC Tuatara (Too-ah-ta-ra) has a twin-turbocharged V8 which puts out up to 1,750hp when it’s running on ethanol, however there is some controversy around the official top speed.
 You see, SSC claimed the Tuatara actually managed 331mph in October 2020 to make it the world’s fastest car — but later admitted to a GPS error that depicted that speed wrongly. Tests in early 2022 saw the car hit an actual top speed of 295mph, not exactly slow.'''),
     Car('Hennessey Venom F5', '272MPH', 'twin-turbocharged V8',
         'assets/images/hor_hennessey.jpg', '1817 hp', '''
 The folks in the USA are smashing the top speed game recently, with fourth place going to another all-American supercar. The Hennessey Venom F5 has a 6.6-litre twin-turbocharged V8 with 1,817hp and 1,617Nm of torque.
 That’s enough to launch it from 0-60mph in 2.6 seconds, and it’ll go onto a top speed of 272mph – for now. Hennessey wants to get even more out of the F5, with the firm targeting a top speed of over 300mph in 2024'''),
-    Car('Rimac Nevera', '256MPH', 'electric motors', 'assets/images/hor_rimac_nevera.jpg',
-        '1813 horsepower', '''
+    Car('Rimac Nevera', '256MPH', 'electric motors',
+        'assets/images/hor_rimac_nevera.jpg', '1813 horsepower', '''
 Electric cars offer instant acceleration thanks to the torque from their electric motors, however the added weight of the batteries can limit them in terms of top speed. This isn’t the case for the Rimac Nevera though.
 This ballistic EV hypercar has four electric motors, producing a whopping 1,914hp and 2,300Nm of torque. That’s enough to launch it from 0-60mph in just 1.9 seconds, and it can go on to a top speed of 256mph. It’s made almost entirely out of carbon fibre to keep the weight down, but this isn’t just some stripped-out drag racer. The interior is a lovely place to be and you still get all the luxuries you’d expect of a car that costs £2 million.'''),
-    Car('McLaren Speedtail', '250MPH', 'Twin-turbocharged V8', 'assets/images/hor_mclaren.jpg',
-        '746 horsepower', '''
+    Car('McLaren Speedtail', '250MPH', 'Twin-turbocharged V8',
+        'assets/images/hor_mclaren.jpg', '746 horsepower', '''
 McLaren didn’t design the Speedtail to chase world records, but, with a 250mph top speed, it’s the fastest road car it has ever built.
 That’s faster than its last three-seat car — the legendary McLaren F1. Its super-slippery bodywork lacks drag-inducing wings and means it looks like something ripped straight from a futuristic cyberpunk film. Just think about carrying two of your mates at that speed…'''),
     Car('Koenigsegg Regera', '250MPH', 'Twin-turbo V8 combustion',
@@ -48,12 +48,12 @@ Unlike any other car on this list, the Regera can go from 0-250mph using just on
         'assets/images/hor_aston_martin.jpg', '1000 hp', '''
 You may not think, based on appearances, that the Aston Martin Valkyrie is road legal. It might look like a full-blown LeMans racer, but you can really drive it on the street.
 Mounted behind the driver is a 6.5-litre hybrid V12 with 1,160hp, capable of pushing this F1 inspired road-racer from 0-60mph in 2.5 seconds. Not really surprising when you realise that it weighs just over a tonne.'''),
-    Car('Pagani Huayra', '238MPH', 'twin-turbocharged M158 60° V12', 'assets/images/hor_pagani.jpg',
-        '730 hp', '''
+    Car('Pagani Huayra', '238MPH', 'twin-turbocharged M158 60° V12',
+        'assets/images/hor_pagani.jpg', '730 hp', '''
 The Pagani Huayra is a fantastic looking car. Every detail is exquisite, and it’s more than just a pin-up as well. It has an AMG derived twin-turbocharged V12 engine putting out 730hp and 1,000Nm of torque, enough for a top speed of 238mph.
 The Huayra is made mostly of carbon fibre to keep the weight down, as well as to make it more rigid, and it’s covered in active aero to help it slip through the air more easily.'''),
-    Car('Lamborghini Revuelto', '217MPH', 'V12 engine', 'assets/images/hor_lambor.jpg',
-        '1000 horsepower', '''
+    Car('Lamborghini Revuelto', '217MPH', 'V12 engine',
+        'assets/images/hor_lambor.jpg', '1000 horsepower', '''
 In a world of downsizing and forced induction, the lunatics at Lamborghini have stuck with a naturally aspirated V12 for its new flagship Revuelto. That’s not to say it’s completely unassisted, as you also get three electric motors to boost the power.
 Specifically, you get a combined 1,015hp, more than any road-going Lamborghini ever made. 0-60mph is dealt with in 2.5 seconds, and it’ll go on to a top speed of 217mph.''')
   ];
@@ -89,8 +89,29 @@ Specifically, you get a combined 1,015hp, more than any road-going Lamborghini e
                         ));
                     break;
                   case 'Exit':
-                    Navigator.pop(context);
-                    exit(0);
+                    showDialog(
+                        context: context,
+                        builder: (BuildContext dialogContext) {
+                          return AlertDialog(
+                            icon: const Icon(Icons.directions_walk),
+                            title: const Text('Are you sure?'),
+                            content: const Text(
+                                'Are you sure you want to exit from app?'),
+                            actions: [
+                              TextButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                    exit(0);
+                                  },
+                                  child: Text('Yes')),
+                              TextButton(
+                                  onPressed: () {
+                                    Navigator.of(dialogContext).pop();
+                                  },
+                                  child: Text('No'))
+                            ],
+                          );
+                        });
                 }
               },
               itemBuilder: (BuildContext context) {
